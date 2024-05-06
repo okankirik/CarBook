620caebb631dbef22bc7299057801077ba19e269
Okan Kırık <okankirik@gmail.com>
2024-05-06 11:02:09 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/CarDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities;
+
+public class CarDescription
+{
+    public int Id { get; set; }
+    public string Detail { get; set; }
+    public int CarId { get; set; }
+    public Car Car { get; set; }
+}
