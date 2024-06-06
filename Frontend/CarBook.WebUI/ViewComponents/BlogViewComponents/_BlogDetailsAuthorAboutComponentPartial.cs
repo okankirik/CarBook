d2d9001ee2ea7d0e53581da0ed5daa572aa04aad
Okan Kırık <okankirik@gmail.com>
2024-06-06 15:44:31 +0300

    "Blog"'un "Yazar" bilgilerini listeleme işlemi tamamlandı.

--- Frontend/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
+++ Frontend/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AuthorDtos;
+using CarBook.Dto.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsAuthorAboutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsAuthorAboutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.blogId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7005/api/Blogs/GetBlogByAuthorId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GetAuthorByBlogAuthorIdDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
